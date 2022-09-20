caa90623532be06b611bf3df579854e01ff0877b
yanasekenta <ynsknt0505@gmail.com>
2022-09-20 18:29:14 +0900

    アニメーションさせてみる

--- src/index.js
+++ src/index.js
@@ -1,44 +1,52 @@
-async function draw () {
+async function draw() {
   const canvas1 = document.getElementById('test1')
-  const ctx1 = canvas1.getContext('2d')
-  const image = ctx1.createImageData(50, 50)
   const canvas2 = document.getElementById('test2')
+  const ctx1 = canvas1.getContext('2d')
   const ctx2 = canvas2.getContext('2d')
-  
-  // for (i=0; i<image.width*image.height*4;i+=4)
-  // {
-  // image.data[i+0]=255;
-  // image.data[i+1]=0;
-  // image.data[i+2]=0;
-  // image.data[i+3]=255;
-  // }
-  
-    for(let i = 0; i < image.data.length; i ++) {
-      image.data[i * 4] = i * 4 * 40
-      image.data[i * 4 + 1] = 155
-      image.data[i * 4 + 2] = 255
-      image.data[i * 4 + 3] = 255
+  const image = ctx1.createImageData(20, 20)
+  const image2 = ctx2.createImageData(20, 20)
+  const image3 = ctx1.createImageData(20, 20)
 
-    // image.data[i * 4] = Math.floor(Math.random() * 256)
-    // image.data[i * 4 + 1] = Math.floor(Math.random() * 256)
-    // image.data[i * 4 + 2] = Math.floor(Math.random() * 256)
-    // image.data[i * 4 + 3] = 255
+  for (let i = 0; i < image.data.length; i++) {
+    image.data[i * 4] = 40
+    image.data[i * 4 + 1] = 155
+    image.data[i * 4 + 2] = 255
+    image.data[i * 4 + 3] = 255
+  }
+
+  for (let i = 0; i < image3.data.length; i++) {
+    image3.data[i * 4] = 255
+    image3.data[i * 4 + 1] = 255
+    image3.data[i * 4 + 2] = 255
+    image3.data[i * 4 + 3] = 255
+  }
+
+  let x = 0
+  let y = 0
+
+  setInterval(function put() {
+    // if(i*20 === image.width) {
+
+    // }
+
+    ctx1.putImageData(image, x, y * 20)
+    if ( y> 0) {
+      ctx1.putImageData(image2, x, (y - 1) * 20)
     }
-    
-  
+    y++
+  }, 100)
 
-  ctx1.putImageData(image, 10, 10)
-  
+
   // 長方形として切り出す部分の指定
   const bitmap = await createImageBitmap(
     image,
-    0, 0, 300, 300
+    0, 0, 100, 100
   )
 
   /**
    * @type {HTMLCanvasElement}
    */
-  
+
   // 画像を貼り付ける
   ctx2.drawImage(bitmap, 0, 0, 300, 300)
 }
